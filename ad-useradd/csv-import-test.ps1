331e9d2dc34abe3bb6700ec24a82c4beab3e0420
ewersonaraujo <wewerson@gmail.com>
2023-05-22 09:13:39 -0300

    Active Directory mass useradd.

---
+++ ad-useradd/csv-import-test.ps1
@@ -0,0 +1,22 @@
+﻿cls
+Remove-Variable * -ErrorAction SilentlyContinue
+
+$nome = @()
+$sobrenome = @()
+$login = @()
+$email = @()
+
+$csv = Import-Csv .\verdanatech.CSV -Delimiter ";" | ForEach-Object {
+
+    $nome += $_.GivenName
+    $sobrenome += $_.Surname
+    $email += $_.Email
+    $login += $_.GivenName+"."+$_.Surname
+}
+
+
+
+$nome[1]
+$sobrenome[1]
+$email[1]
+$login[1].ToLower()
---
+++ ad-useradd/loop-array-test.ps1
@@ -0,0 +1,10 @@
+﻿cls
+Remove-Variable * -ErrorAction SilentlyContinue
+$nome = @('Ewerson', 'Gabriela')
+$sobrenome = @('Araujo', 'Silva')
+
+for($i = 0; $i -le 1; $i++){
+
+    $nome[$i]+" "+$sobrenome[$i]
+
+}
