d4eb3a0dacd9b6b42729fa568ca23f5bbfea4516
Ewerson Araujo <ewerson.araujo@outlook.com>
2023-11-02 10:34:44 -0300

    editing

--- PSWinUpdate/WinUpdate.ps1
+++
@@ -1,1 +0,0 @@
-@((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search("IsHidden=0 and IsInstalled=0").Updates) | Select-Object Title | Measure-Object | Select-Object -ExpandProperty Count
